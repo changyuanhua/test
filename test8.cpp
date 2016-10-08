cdd3f3ec6333a5108856c8599c4c0dfa1ad8b45b
changyuanhua <chang19960726@gmail.com>
2016-10-08 21:41:16 +0800

    改test8

--- test8.cpp
+++ test8.cpp
@@ -1,34 +1,16 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
-  int i,j,k,a,b,x=-1,y[100]={0},z=0;
-  cin >> i;
-  for(j=2;j<=i;j++)
-  {
-    k=i%j;
-    x++;
-    if(k==0 && i!=j)
-    {
-      z=1; 
-      for(a=2;a<=j;a++)
-      {
-        b=j%a;
-	if(b==0)
-	{
-	    y[x]=a;
-	    cout << y[x] <<endl;
-	  
-        }
-      }
-    }
-    else if (k==0 && i==j && z==0)
-    {
-      cout << j;
-    }
-
-  }
-  return 0;
-}  
+	int n,i;
+		cout<<"n=";
+			cin>>n;
+				for(i=2;i<=n;i++)
+						if(n%i==0)
+								{
+											while(n%i==0)
+															n/=i;
+																		cout<<endl<<i;
+																				}
+																					return 0;
+																					}
